6b2104a6f436764a3ba6571762a2c43445f29439
webwithmona <webwithmona@gmail.com>
2026-01-28 18:07:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const reveals = document.querySelectorAll(".reveal");
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if(entry.isIntersecting){
+      entry.target.classList.add("active");
+    }
+  });
+},{ threshold:0.1 });
+
+reveals.forEach(el => observer.observe(el));
